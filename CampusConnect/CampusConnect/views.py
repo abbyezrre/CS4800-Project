3513ba7f5c353f9171122da64b954dd1a3c9722c
budgu <73621809+budgu@users.noreply.github.com>
2023-11-01 00:35:14 -0700

    working on sign in functionality

--- CampusConnect/CampusConnect/views.py
+++ CampusConnect/CampusConnect/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def map(request):
     template = "Map/map.html"
@@ -8,8 +11,43 @@ def map(request):
 def home(request):
     return render(request, "index.html")
 
-def signup(request):
-    return render(request, "signup.html")
-
 def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "index.html", {'fname': fname})
+            
+        else:
+            messages.error(request, "Invalid Credentials")
+        
     return render(request, "signin.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        
+        myuser.save()
+        
+        messages.success(request, "Your account has been created.")
+        
+        #TO DO: redirect back to signin on submitting form
+        return redirect('signin')
+        
+        
+    return render(request, "signup.html")
