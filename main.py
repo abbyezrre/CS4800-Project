7b214444af4b17e9d3b4268c3dbccae4033e9594
budgu <73621809+budgu@users.noreply.github.com>
2023-11-07 18:35:35 -0800

    fixing login and user class so that they pull from database instead of fakeB

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
+from Database.pullingUserInfo import *
+from Database.StoringUserInfo import *
 
 from login import *
 from user import *
 
 
 def main():
-    """
-    demo1 login test using dictionary:         
-            "johnsmith209" : "Password",
-            "budgie" : "parakeet1",
-            "superc0co" : "Isaac123",
-            "abbyeee" : "h00ray!", 
-            "kim" : "2bunny",
-            "elvin" : "basketB"
-    """
 
     #login testing - helen
     loginTest = loginPage()
     loginTest.login()
+
 
 
 if __name__ == "__main__":
--- user.py
+++ user.py
@@ -1,11 +1,12 @@
 #helen
+from Database.pullingUserInfo import *
 
 # this class creates the user object and stores username, pass, email, etc.
 class User:
     def __init__(self):
         self.firstname = ""
         self.lastname = ""
-        self.middlename = ""
+        self.fullname = ""
         self.username = ""
         self.password = ""
         self.user_email = ""
@@ -13,33 +14,26 @@ class User:
         self.signup_date = -99
         self.profile_name = ""
         self.bio = ""
-        
-        #creates a test dictionary of usernames and passwords for login functionality
-        self.validUsers = {
-            "johnsmith209" : "Password",
-            "budgie" : "parakeet1",
-            "superc0co" : "Isaac123",
-            "abbyeee" : "h00ray!", 
-            "kim" : "2bunny",
-            "elvin" : "basketB"
-        }
+        self.database = pullingUserInfo()
         
     #finds the user if key matches and updates values and returns true, otherwise returns false
 
-    def findUser(self, key):
-        #TODO: Update with correct database function. Right now it is pulling from a made up username list in FakeB
-        if key in self.validUsers.keys():
-            #TODO: needs to be uncommented out when we have a full database
-            #self.firstname = self.database.get_firstname()
-            #self.lastname = self.database.get_lastname()  
-            #self.middlename = self.database.get_middlename()
-            self.username = key
-            self.password = self.validUsers[key]
-            #self.user_email = self.database.get_useremail()
-            #self.age = self.database.get_age()
-            #self.signup_date = self.database.get_signin_date()
-            #self.profile_name = self.database.get_profile_name()
-            #self.bio = self.database.get_user_bio()
+    def findUser(self, username):
+
+        if username in self.database.get_allFieldInfo('username'):
+
+            self.firstname = self.database.get_firstname(username)
+            self.lastname = self.database.get_lastname(username)  
+            self.fullname = self.database.get_fullname(username)
+            self.username = self.database.get_username(username)
+            self.password = self.database.get_password(username)
+            #self.user_email = self.database.get_useremail(username)
+            self.age = self.database.get_age(username)
+            #self.signup_date = self.database.get_signin_date(username)
+            #self.profile_name = self.database.get_profile_name(username)
+            
+            #error with retrieving bio
+            #self.bio = self.database.get_bio(username)
             return True
         else:
             return False
@@ -53,15 +47,11 @@ class User:
     
     # get full name
     def get_fullname(self):
-        return self.firstname + " " + self.middlename + " " + self.lastname
+        return self.firstname + " " + self.lastname
     
     # get only first name
     def get_firstname(self):
         return self.firstname
-    
-    # get only middle name
-    def get_middlename(self):
-        return self.middlename
     
     # get only last name
     def get_lastname(self):
@@ -90,22 +80,6 @@ class User:
     # get name of profile
     def get_profile_name(self):
         return self.profile_name
-    
-    # get user post comment
-    def get_user_post_comment(self):
-        return self.user_post_comment
-    
-    # get date of posted comment
-    def get_comment_date(self):
-        return self.comment_date
-    
-    # get image posted (right now its a string but should be a pdf? pgn? discuss as a group)
-    def get_post_image(self):
-        return self.post_image
-    
-    # get video posted (right now its a string but should be a vid? discuss as a group)
-    def get_post_video(self):
-        return self.post_video
     
     def get_user_bio(self):
         return self.bio
