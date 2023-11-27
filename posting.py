c72a2b5793a10de68adfe3f881706fc2ef0d8c24
Kimberly Encinas <kimbencinas@gmail.com>
2023-11-27 13:40:46 -0800

    post controller changes

--- posting.py
+++ posting.py
@@ -1,11 +1,16 @@
 from user import *
+from Database.StoringUserInfo import *
+from Database.pullingUserInfo import *
 
 class posting:
     def __init__(self):
         self.user_post = []
+        self.storing_post = storingPost()
+        self.pulling_post = pullingPostInfo()
     
     def post_feed(self, name, message):
         self.user_post.append({'name': name, 'message': message})
 
     def get_user_post(self):
         return self.user_post
+    
