a478daf12644e63c98863e86678dce3716f44fc1
budgu <73621809+budgu@users.noreply.github.com>
2023-12-06 00:10:17 -0800

    fixing html for home and search, adding onto search controller so it can search for clubs

--- app.py
+++ app.py
@@ -7,67 +7,65 @@ from Database.StoringUserInfo import *
 from Database.pullingUserInfo import *
 from search import *
 from posting import PostingController
-from clubs import ClubsController
+from login import *
+from signup import *
 # create the application object
 app = Flask(__name__)
-
-# use decorators to link the function to a url
+app.secret_key = 'super secret key'
 
 @app.route('/')
 def home():
-    return render_template('home.html', user_post=session.get('user_post'))  # render a template
+    user = None
+    if session.get('username') is not None:
+        user = session.get('username')
+        username = User()
+        username.findUser(user)
+        user = username.firstname
+        
+    return render_template('home.html',user=user, user_post=session.get('user_post'))  # render a template
 
 
 #Helen - signin
 @app.route('/signin', methods=['GET', 'POST'])
 def login():
     error = None
-<<<<<<< HEAD
-    if request.method == 'POST':
-        #creating user object
-        user = User()
-        #made username variable global - Elvin
-        global username 
-=======
-    user= None
+
     global username
     if request.method == 'POST':
         
         
->>>>>>> fb70b7bed65a2d05232431274408ecad2dce5ab6
         #pulling info from html input
         username = request.form.get("username")
         password = request.form.get("password")
-        
-        #returns true or false if user exists in database
-        verifyUser = user.findUser(username)
-        
-        if verifyUser is True:
-            #redirects to homepage if passwords match
-            if user.get_password() == password:
-                return redirect(url_for('home'))
-            else:
-                error = 'Invalid Password. Please try again.'
+
+        login = loginPage(username, password)
+        login.checkpassword()
+        if login.loginStatus is True: 
+            session['username'] = username
+            return redirect(url_for('home'))
         else:
-            error = 'Invalid Username. Please try again.'
-                
+            error = login.error
+
+
     return render_template('signin.html', error=error)
 
  # map page - Abigail 
- # connects to map controller and the html        
+ # connects to map controller and the html     
+ # made changes to map function by implementing some parts of Issac's code so image pops on the page
 @app.route('/map', methods= ['GET', 'POST'])
 def map():
+    map = Map()
     
     if request.method == 'POST':
-        map = Map()
+        
     
         roomnumber = request.form.get("roomnumber")
         roomImage = map.searchMap(roomnumber)
+
+    else:
+        roomImage = None
         
-        if roomImage is True:
-            return render_template('map.html', img = roomImage)
-    
-    return render_template('map.html')
+    return render_template('map.html', img = roomImage)
 
 #helen - search function
 #needs the UI to be updated 
@@ -85,68 +83,79 @@ def search():
         #appends array with first name and last name of user
         output.append(searching.output)
         input += searchInput
+ 
+        if searching.clubValid == True:
+            clubImage = searching.getClubImage(searchInput)
+        else:
+            clubImage = None
         
     output = sum(output,[])
-    return render_template('search.html', output=output, input=input, len = len(output))
+    return render_template('search.html', output=output, input=input, len = len(output), img=clubImage)
+#helen - sign out
+#clears session user
+@app.route('/signout', methods=['GET', 'POST'])
+def signout():
+    user=None
+    session.clear()
+    return render_template('home.html',user=user)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     #copied from signup.py, coded by Abby
     #modifications by Helen for connecting the controller to Flask
     #modifications by Helen to check if user exists in the database before user can sign up, and to check if passwords match
+    #modifications by Helen to turn it into a class
     error = None
     if request.method == 'POST':
-        
-        #create a new user
-        newUser = StoringUserInfo()
-        #create database of usernames
-        usernameList = pullingUserInfo().get_allFieldInfo('username')
-        
-        #gets input from html and stores it as username, pass1, and pass2
+        #gets input from html boxes
         username = request.form.get("username")
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
         pass1 = request.form.get("pass1")
         pass2 = request.form.get("pass2")
+        email = request.form.get("email")
+        signup = signupPage(username, fname, lname, pass1, pass2, email)
         
-        #checks to see if username is taken
-        if username in usernameList:
-            error = 'Username is taken. Try again'
+        signup.signup()
+        
+        if signup.signupStatus == True:
+            return redirect(url_for('login'))
+        
         else:
-            newUser.set_username(username)
-            newUser.set_firstname(request.form.get("fname"))
-            newUser.set_lastname(request.form.get("lname"))
-            #checks to see if passwords match
-            if pass1 == pass2:
-                newUser.set_password(request.form.get("pass1"))
-                newUser.create_new_document()
-                #redirects to login page
-                return redirect(url_for('login'))
-            else:
-                error = "Passwords don't match. Try again"
+            error = signup.error
         
-        #TO DO: uncomment out after database can store email
-        #newUser.set_(request.form.get("email"))
-    
+
     return render_template('signup.html', error=error)
-    
-#kim - post function
+
+
+# ... (previous code)
+
+# Posting route - Isaac
 @app.route('/posting', methods=['GET', 'POST'])
-def post():
-    user_post = session.get('user_post', [])
+def posting():
+    # Instantiate the PostingController class
+    posting_controller = storingPost()
+    post = pullingPostInfo()
 
     if request.method == 'POST':
-        #get data from form
-        name = request.form.get("name")
-        message = request.form.get("message")
+        # Get the post content from the form
+        post_content = request.form.get('postContent')
 
-        posting_post = PostingController()
+        # Set the user and comment in the PostingController instance
+        posting_controller.set_user("Isaac Flores")
+        posting_controller.set_comment(post_content)
 
-        posting_post.post_feed(name, message)
-        #append post to user posts
-        user_post.append({'name': name, 'message': message})
-        #session['user_post'] = user_post
+        # Create a new document in the MongoDB collection
+        posting_controller.create_new_document()
+
+    
+    # Retrieve the last 5 posts from the database
+    last_5_posts = post.get_last_5_documents()
+
+    # Render the posting.html template and pass the posts to it
+    return render_template('posting.html', posts=last_5_posts)
 
 
-    return render_template('posting.html', user_post=user_post)
 
 # profile function - Elvin
 @app.route('/profile', methods=['GET', 'POST'])
@@ -160,8 +169,6 @@ def profile():
     major = None
     bio = None
 
-<<<<<<< HEAD
-=======
     #creates profile class
     user = Profile(username)
 
@@ -225,19 +232,16 @@ def display_map():
     return render_template('map.html', mapimage=map_image_data)
 """
 
->>>>>>> fb70b7bed65a2d05232431274408ecad2dce5ab6
 @app.route('/clubs')
 def index():
-    clubs_controller = ClubsController()
-    clubs = clubs_controller.get_clubs_info()
-
-   # clubs = [
+    user_info = pullingClubInfo()
+    clubs = [
         #pulling the infomation from database of clubs to the website
-        #{'name': user_info.get_club("Computer Science") , 'description': user_info.get_eventinfo("Computer Science") , 'contact': user_info.get_contact("Computer Science"), 'logo': user_info.get_image("Computer Science")},
-        #{'name': user_info.get_club("Gaming") , 'description': user_info.get_eventinfo("Gaming") , 'contact': user_info.get_contact("Gaming"), 'logo': user_info.get_image("Gaming")},
+        {'name': user_info.get_club("Computer Science") , 'description': user_info.get_eventinfo("Computer Science") , 'contact': user_info.get_contact("Computer Science"), 'logo': user_info.get_image("Computer Science")},
+        {'name': user_info.get_club("Gaming") , 'description': user_info.get_eventinfo("Gaming") , 'contact': user_info.get_contact("Gaming"), 'logo': user_info.get_image("Gaming")},
   
         # Add more clubs as needed
-    #]
+    ]
     return render_template('clubs.html', clubs=clubs)
    
 # start the server with the 'run()' method
