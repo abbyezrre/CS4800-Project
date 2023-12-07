e5d731a27b5455cfdb9608cde1d83623f3b31b05
budgu <73621809+budgu@users.noreply.github.com>
2023-12-06 19:02:30 -0800

    adding comments to app.py, login.py, and search.py to explain how functions are working

--- login.py
+++ login.py
@@ -8,13 +8,16 @@ class loginPage:
         self.username = username
         self.password = password
         self.error = None
-        
+    
+    #funciton to check if passwords are a match
     def checkpassword(self):
-        
+        #returns a bool if username is found
         verifyUser = self.user.findUser(self.username)
         
+        #if user is found, then it sets login status as true
+        #else it will set error to the error message
         if verifyUser:
-    
+            
             if self.user.get_password() == self.password:
                 self.loginStatus = True
             else:
--- search.py
+++ search.py
@@ -12,27 +12,30 @@ class searchBar:
         self.userValid = False
 
         
-    #for now, it just searches for usernames in the database and returns their first + last name
+    #searches usernames and clubs (Case sensitive)
     def search(self):
-
+        #returns a bool if the search is a username
         self.userValid = self.user.findUser(self.query)
+        #creates club db class
         club = pullingClubInfo()
+        #returns the club if search matches, else clubExists = None
         clubExists = club.get_club(self.query)
 
-        
+        #if the search was a username, the full name of the user is appended tothe output
         if self.userValid == True:
             self.output.append(self.user.get_fullname())
 
-            
+        #else if the club is a match, then the club name and contact info is added
         elif clubExists is not None:
             self.output.append(club.get_club(self.query) + " Club")
             self.output.append(club.get_contact(self.query))
             self.clubValid = True
+        
+        #else no results are found
         else:
-            
             self.output.append("No results found")
 
-            
+    #returns club image if the club is found     
     def getClubImage(self, club):
         getClubImage = pullingClubInfo()
         if self.clubValid is True:
